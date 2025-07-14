d6503e962cf67774787b7e55a34cb618462b62ae
jonahhess <jonahhessdev@gmail.com>
2025-07-14 23:04:00 +0300

    added some fn signatures

---
+++ contact-manager/utils/fileUtils.js
@@ -0,0 +1,7 @@
+const fs = require("node:fs");
+
+const readFromFile = (fileName) => 0;
+const saveToFile = (fileName, data) => 0;
+const isExistingFile = (fileName) => 0;
+const createNewFile = (fileName) => 0;
+// const deleteFile = (fileName) => 0;
