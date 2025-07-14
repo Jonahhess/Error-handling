50bd7a09e39026c7744af16ed191c624b09c48b3
jonahhess <jonahhessdev@gmail.com>
2025-07-14 21:32:36 +0300

    added internal validation, exported parsers

--- contact-manager/utils/validation.js
+++ contact-manager/utils/validation.js
@@ -2,4 +2,18 @@ const parseAdd = (args) => 0;
 const parseDelete = (args) => 0;
 const parseSearch = (args) => 0;
 
+const validateName = (name) => {
+  return true; // so far all names are true
+};
+
+const validateEmail = (email) => {
+  return email.match(
+    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  );
+};
+
+const validatePhone = (phone) => {
+  return /^\d+$/.test(phone);
+};
+
 module.exports = {};
