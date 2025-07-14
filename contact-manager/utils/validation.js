7255a439318b907444f252688707a0eac8b46be7
jonahhess <jonahhessdev@gmail.com>
2025-07-14 22:37:43 +0300

    filled in parse add, added error values

--- contact-manager/utils/validation.js
+++ contact-manager/utils/validation.js
@@ -1,4 +1,15 @@
-const parseAdd = (args) => 0;
+const parseAdd = (args) => {
+  const [name, email, phone] = args;
+  try {
+    validateName(name);
+    validateEmail(email);
+    validatePhone(phone);
+  } catch (err) {
+    console.error("Could not parse arguments for add function");
+    throw err;
+  }
+  return [name, email, phone];
+};
 const parseDelete = (args) => 0;
 const parseSearch = (args) => 0;
 
@@ -7,13 +18,15 @@ const validateName = (name) => {
 };
 
 const validateEmail = (email) => {
-  return email.match(
-    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  return (
+    email.match(
+      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    ) || Error("Invalid Email")
   );
 };
 
 const validatePhone = (phone) => {
-  return /^\d+$/.test(phone);
+  return /^\d+$/.test(phone) || Error("Invalid Phone Number");
 };
 
 module.exports = { parseAdd, parseDelete, parseSearch };
