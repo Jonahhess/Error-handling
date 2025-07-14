f44eedac8636013be423ad225e1bbf93cc22c44d
jonahhess <jonahhessdev@gmail.com>
2025-07-14 23:12:59 +0300

    changed fn name to better reflect use

--- contact-manager/utils/fileUtils.js
+++ contact-manager/utils/fileUtils.js
@@ -8,7 +8,14 @@ const readFromFile = (fileName) => {
     console.error(err);
   }
 };
-const saveToFile = (fileName, data) => 0;
+const AppendToFile = (fileName, data) => {
+  try {
+    fs.writeFileSync(fileName, data, { flag: "a" });
+    // file written successfully
+  } catch (err) {
+    console.error(err);
+  }
+};
 const isExistingFile = (fileName) => 0;
 const createNewFile = (fileName) => 0;
 // const deleteFile = (fileName) => 0;
