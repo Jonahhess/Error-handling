0ad1ea7fbaadff1bcab15b35d8342d0b27df42a8
itaiglikman <itaig1998@gmail.com>
2025-07-14 21:30:45 +0300

    feat: implement initial ContactService class structure

---
+++ contact-manager/services/contactService.js
@@ -0,0 +1,36 @@
+import fs from 'fs';
+
+class ContactService {
+
+    controller() {
+
+    }
+
+    dbAddress = '../DB/contacts.json';
+
+    load() {
+        
+    }
+
+    add(argsArr) {
+        
+    }
+
+    delete(email) {
+
+    }
+
+    list() {
+
+    }
+
+    search(name) {
+
+    }
+
+
+
+
+}
+
+export default ContactService();
