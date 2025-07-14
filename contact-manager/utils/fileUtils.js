22c40724cab403552ecfe1eb5cd3168ea22328d9
jonahhess <jonahhessdev@gmail.com>
2025-07-14 23:10:04 +0300

    read implementation

--- contact-manager/utils/fileUtils.js
+++ contact-manager/utils/fileUtils.js
@@ -1,6 +1,13 @@
 const fs = require("node:fs");
 
-const readFromFile = (fileName) => 0;
+const readFromFile = (fileName) => {
+  try {
+    const data = fs.readFileSync(fileName, "utf8");
+    return data;
+  } catch (err) {
+    console.error(err);
+  }
+};
 const saveToFile = (fileName, data) => 0;
 const isExistingFile = (fileName) => 0;
 const createNewFile = (fileName) => 0;
