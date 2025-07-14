876418759697ab33a5fcd889278f749d0685da12
jonahhess <jonahhessdev@gmail.com>
2025-07-14 22:43:57 +0300

    validation fns now throw errors rather than return them

--- contact-manager/utils/validation.js
+++ contact-manager/utils/validation.js
@@ -18,15 +18,22 @@ const validateName = (name) => {
 };
 
 const validateEmail = (email) => {
-  return (
-    email.match(
-      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    ) || Error("Invalid Email")
+  const validEmail = email.match(
+    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
   );
+  if (!validEmail) {
+    throw new Error("Invalid email");
+  }
+
+  return validEmail;
 };
 
 const validatePhone = (phone) => {
-  return /^\d+$/.test(phone) || Error("Invalid Phone Number");
+  const validPhone = /^\d+$/.test(phone);
+  if (!validPhone) {
+    throw new Error("Invalid Phone Number");
+  }
+  return validPhone;
 };
 
 module.exports = { parseAdd, parseDelete, parseSearch };
