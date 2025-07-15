65d695d665a7f48b76ffc9c389549ccbdf098b3f
jonahhess <jonahhessdev@gmail.com>
2025-07-15 09:56:55 +0300

    separated out internal fns

---
+++ contact-manager/utils/throws.js
@@ -0,0 +1,15 @@
+// internal fn
+const throwIfNotEqual = (a, b, errorMsg) => {
+  if (a !== b) {
+    throw new Error(errorMsg);
+  }
+};
+
+// internal fn
+const throwIfNaN = (string, errorMsg) => {
+  if (Number.isNaN(Number.parseInt(string))) {
+    throw new Error(errorMsg);
+  }
+};
+
+module.exports = { throwIfNotEqual, throwIfNaN };
