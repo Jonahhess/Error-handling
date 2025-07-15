35e6a50ecbf852f0602922204a7d0e031a97c80e
jonahhess <jonahhessdev@gmail.com>
2025-07-15 14:21:09 +0300

    wrapped utils in try catch

--- contact-manager/utils/jsonUtils.js
+++ contact-manager/utils/jsonUtils.js
@@ -1,4 +1,20 @@
-const JSONToObjectArray = (json) => JSON.parse(json);
-const ObjectArrayToJSON = (objectArray) => JSON.stringify(objectArray);
+const JSONToObjectArray = (json) => {
+  try {
+    const parsedJSON = JSON.parse(json);
+    return parsedJSON;
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+};
+const ObjectArrayToJSON = (objectArray) => {
+  try {
+    const stringifiedObjectArray = JSON.stringify(objectArray);
+    return stringifiedObjectArray;
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+};
 
 module.exports = { JSONToObjectArray, ObjectArrayToJSON };
