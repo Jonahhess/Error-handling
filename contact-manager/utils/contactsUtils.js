ff9ce7eab909c520ae1cbc4b27165443380fe851
jonahhess <jonahhessdev@gmail.com>
2025-07-16 15:01:25 +0300

    improved search algorithm

--- contact-manager/utils/contactsUtils.js
+++ contact-manager/utils/contactsUtils.js
@@ -1,21 +1,19 @@
-
 // change to the better version
-const findAllContactsByName = (contacts, searchName) =>
-    contacts.filter(c => {
-        searchName = searchName.toLowerCase();
-        const contactName = c.name.toLowerCase();
-        const [contactFirstName, contactLastName] = contactName.split(" ");
-        return ([contactName, contactFirstName, contactLastName].includes(searchName));
-    });
+const findAllContactsByName = (contacts, query) =>
+  //
+  {
+    const queryArr = query.split(" ");
+    return contacts.filter((c) => queryArr.every((q) => c.name.includes(q)));
+  };
 
 const objectArrayToMap = (objArr) => {
-    const map = new Map();
-    objArr.forEach(contact => {
-        map.set(contact.email, contact);
-    });
-    return map;
+  const map = new Map();
+  objArr.forEach((contact) => {
+    map.set(contact.email, contact);
+  });
+  return map;
 };
 
-const mapToObjectArray = (map) => Array.from(map.values());;
+const mapToObjectArray = (map) => Array.from(map.values());
 
 export default { findAllContactsByName, objectArrayToMap, mapToObjectArray };
