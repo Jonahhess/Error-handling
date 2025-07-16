6f53de7e0dbff2fbb09906154f931ba7e3ffe6c4
itaiglikman <itaig1998@gmail.com>
2025-07-16 12:45:25 +0300

    change: from commonJS to ES6. use view only in commandService.

--- contact-manager/services/commandService.js
+++ contact-manager/services/commandService.js
@@ -1,56 +1,76 @@
+import ContactService from './contactService.js';
+import view from "../commands/commandHandler.js";
+
 function handleAdd(name, email, phone) {
-  try {
-    const updatedData = model.add(name, email, phone);
-    view.printAdded(name);
-    return updatedData;
-  } catch (error) {
-    view.printError(error);
-  }
+    try {
+        const updatedData = ContactService.add(name, email, phone);
+        view.printAdded(name);
+        return updatedData;
+    } catch (error) {
+        view.printError(error);
+    }
 }
 
 function handleDelete(email) {
-  try {
-    const [updatedData, deletedName] = model.delete(email);
-    view.printDeleted(deletedName);
-    return updatedData;
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
+    try {
+        const [updatedData, deletedName] = ContactService.deleteContact(email);
+        view.printDeleted(deletedName);
+        return updatedData;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
 }
 
 function handleSearch(query, parsedData) {
-  const contacts = model.search(query, parsedData);
-  return contacts;
+    const contacts = ContactService.search(query, parsedData);
+    view.printSearch(contacts);
 }
 
 function handleLoad() {
-  view.printLoading();
-  let parsedData;
-  try {
-    parsedData = model.load();
-    view.printLoaded(data.length);
-  } catch (error) {
-    console.error(error);
-    view.printFileNotFound();
-    model.createFile(); // instead of throwing, we gracefully correct the program
-  }
-  return parsedData;
+    view.printLoading();
+    let parsedData;
+    try {
+        parsedData = ContactService.load();
+        view.printLoaded(data.length);
+    } catch (error) {
+        console.log("here");
+        console.error(error);
+        view.printFileNotFound();
+        ContactService.createFile(); // instead of throwing, we gracefully correct the program
+        parsedData = [];
+    }
+    return parsedData;
 }
 
 function handleSave(updatedData) {
-  try {
-    model.save(updatedData);
-    view.printSaved();
-  } catch (error) {
-    view.printError(error);
-  }
+    try {
+        ContactService.save(updatedData);
+        view.printSaved();
+    } catch (error) {
+        view.printError(error);
+    }
 }
 
-module.exports = {
-  handleAdd,
-  handleDelete,
-  handleSearch,
-  handleLoad,
-  handleSave,
+function handleList(contacts) {
+    view.printList(contacts);
+}
+
+function handleHelp() {
+    view.printHelp();
+}
+
+function handleError(error) {
+    view.printError(error);
+}
+
+export default  {
+    handleAdd,
+    handleDelete,
+    handleSearch,
+    handleLoad,
+    handleSave,
+    handleList,
+    handleHelp,
+    handleError,
 };
