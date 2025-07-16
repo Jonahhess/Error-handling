8620aee50b470fb21712aecb3f7951631cf2c29d
jonahhess <jonahhessdev@gmail.com>
2025-07-16 15:02:37 +0300

    moved  error handling into file

--- contact-manager/services/contactService.js
+++ contact-manager/services/contactService.js
@@ -1,62 +1,72 @@
-import filesUtils from '../utils/fileUtils.js';
-import jsonUtils from '../utils/jsonUtils.js';
-import contactsUtils from '../utils/contactsUtils.js';
+import filesUtils from "../utils/fileUtils.js";
+import jsonUtils from "../utils/jsonUtils.js";
+import contactsUtils from "../utils/contactsUtils.js";
 
 function load() {
-    const json = filesUtils.readFromFile();
-    if (!json) filesUtils.overwriteFile('[]');
-    const parsedData = jsonUtils.JSONToObjectArray(json || '[]');
-    return { parsedData, createdNewArr: !!json };
+  let json;
+  let createdNewArr = false;
+  try {
+    json = filesUtils.readFromFile();
+  } catch (error) {
+    filesUtils.overwriteFile("[]");
+    json = "[]";
+    createdNewArr = true;
+  }
+
+  const parsedData = jsonUtils.JSONToObjectArray(json);
+  return { parsedData, createdNewArr };
 }
 
 function save(parsedData) {
-    const json = jsonUtils.ObjectArrayToJSON(parsedData);
-    filesUtils.overwriteFile(json);
+  const json = jsonUtils.ObjectArrayToJSON(parsedData);
+  filesUtils.overwriteFile(json);
 }
 
 // error handling:
 // 1. handle empty arr
 // 2. handle writing to the file went wrong
 function add(name, email, phone, parsedData) {
-    const contactsMap = contactsUtils.objectArrayToMap(parsedData);
-    if (contactsMap.has(email))
-        throw new Error("✗ Error: Contact with this email already exists");
-    const newContact = { name, email, phone }
-    parsedData.push(newContact);
-    return [name, parsedData];
+  const contactsMap = contactsUtils.objectArrayToMap(parsedData);
+
+  if (contactsMap.has(email))
+    throw new Error("Contact with this email already exists");
+
+  const newContact = { name, email, phone };
+  parsedData.push(newContact);
+  return parsedData;
 }
 
 // error handling:
 // 1. handle writing to the file went wrong
 function deleteContact(email, parsedData) {
-    let contactsMap = contactsUtils.objectArrayToMap(parsedData);
-    const contactToDelete = contactsMap.get(email);
-    if (!contactToDelete)
-        throw new Error("✗ Error: No contact found with email: " + email);
-    const updatedData = contactsUtils.mapToObjectArray(contactsMap);
-    return [contactToDelete.name, updatedData]; // on success - return contact name and contacts length
+  if (!parsedData.length) {
+    return [parsedData];
+  }
+
+  let contactsMap = contactsUtils.objectArrayToMap(parsedData);
+  const contactToDelete = contactsMap.get(email);
+  if (!contactToDelete)
+    throw new Error("No contact found with email: " + email);
+  contactsMap.delete(email);
+  const updatedData = contactsUtils.mapToObjectArray(contactsMap);
+  return [updatedData, contactToDelete.name]; // on success - return contact name and contacts length
 }
 
 function list() {
-    const contacts = this.load();
-    return contacts;
+  const contacts = this.load();
+  return contacts;
 }
 
 function search(name, contacts) {
-    const contactsByName = contactsUtils.findAllContactsByName(contacts, name);
-    return contactsByName;
-}
-
-function createFile() {
-    filesUtils.overwriteFile('[]');
+  const contactsByName = contactsUtils.findAllContactsByName(contacts, name);
+  return contactsByName;
 }
 
 export default {
-    load,
-    save,
-    deleteContact,
-    add,
-    list,
-    search,
-    createFile,
+  load,
+  save,
+  deleteContact,
+  add,
+  list,
+  search,
 };
