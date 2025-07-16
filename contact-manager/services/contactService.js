a6dee436744390a8485c7fbb91fee4a60b0a1aad
itaiglikman <itaig1998@gmail.com>
2025-07-16 12:45:58 +0300

    change: from commonJS to ES6. modify from class to functions

--- contact-manager/services/contactService.js
+++ contact-manager/services/contactService.js
@@ -1,54 +1,62 @@
-const filesUtils = require('../utils/fileUtils');
-const jsonUtils = require('../utils/jsonUtils');
-const contactsUtils = require('../utils/contactsUtils');
-const fs = require('node:fs');
-class ContactService {
+import filesUtils from '../utils/fileUtils.js';
+import jsonUtils from '../utils/jsonUtils.js';
+import contactsUtils from '../utils/contactsUtils.js';
 
-    load() {
-        const json = filesUtils.readFromFile();
-        if (!json) filesUtils.overwriteFile('[]');
-        const parsedData = jsonUtils.JSONToObjectArray(json || '[]');
-        return { parsedData, createdNewArr: !!json };
-    }
-
-    save(parsedData) {
-        const json = jsonUtils.ObjectArrayToJSON(parsedData);
-        filesUtils.overwriteFile(json);
-    }
-
-    // error handling:
-    // 1. handle empty arr
-    // 2. handle writing to the file went wrong
-    add(name, email, phone, parsedData) {
-        const contactsMap = contactsUtils.objectArrayToMap(parsedData);
-        if (contactsMap.has(email))
-            throw new Error("✗ Error: Contact with this email already exists");
-        const newContact = { name, email, phone }
-        parsedData.push(newContact);
-        return [name, parsedData];
-    }
-
-    // error handling:
-    // 1. handle writing to the file went wrong
-    delete(email, parsedData) {
-        let contactsMap = contactsUtils.objectArrayToMap(parsedData);
-        const contactToDelete = contactsMap.get(email);
-        if (!contactToDelete)
-            throw new Error("✗ Error: No contact found with email: " + email);
-        const updatedData = contactsUtils.mapToObjectArray(contactsMap);
-        return [contactToDelete.name, updatedData]; // on success - return contact name and contacts length
-    }
-
-    list() {
-        const contacts = this.load();
-        return contacts;
-    }
-
-    search(name, contacts) {
-        const contactsByName = contactsUtils.findAllContactsByName(contacts, name);
-        return contactsByName;
-    }
-
+function load() {
+    const json = filesUtils.readFromFile();
+    if (!json) filesUtils.overwriteFile('[]');
+    const parsedData = jsonUtils.JSONToObjectArray(json || '[]');
+    return { parsedData, createdNewArr: !!json };
 }
 
-export default ContactService();
+function save(parsedData) {
+    const json = jsonUtils.ObjectArrayToJSON(parsedData);
+    filesUtils.overwriteFile(json);
+}
+
+// error handling:
+// 1. handle empty arr
+// 2. handle writing to the file went wrong
+function add(name, email, phone, parsedData) {
+    const contactsMap = contactsUtils.objectArrayToMap(parsedData);
+    if (contactsMap.has(email))
+        throw new Error("✗ Error: Contact with this email already exists");
+    const newContact = { name, email, phone }
+    parsedData.push(newContact);
+    return [name, parsedData];
+}
+
+// error handling:
+// 1. handle writing to the file went wrong
+function deleteContact(email, parsedData) {
+    let contactsMap = contactsUtils.objectArrayToMap(parsedData);
+    const contactToDelete = contactsMap.get(email);
+    if (!contactToDelete)
+        throw new Error("✗ Error: No contact found with email: " + email);
+    const updatedData = contactsUtils.mapToObjectArray(contactsMap);
+    return [contactToDelete.name, updatedData]; // on success - return contact name and contacts length
+}
+
+function list() {
+    const contacts = this.load();
+    return contacts;
+}
+
+function search(name, contacts) {
+    const contactsByName = contactsUtils.findAllContactsByName(contacts, name);
+    return contactsByName;
+}
+
+function createFile() {
+    filesUtils.overwriteFile('[]');
+}
+
+export default {
+    load,
+    save,
+    deleteContact,
+    add,
+    list,
+    search,
+    createFile,
+};
