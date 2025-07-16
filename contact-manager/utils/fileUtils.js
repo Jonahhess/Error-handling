858447569ad0368eb00e7675c96c6e4ecb0ca432
itaiglikman <itaig1998@gmail.com>
2025-07-16 11:35:53 +0300

    improve: readfile action to support file not found

--- contact-manager/utils/fileUtils.js
+++ contact-manager/utils/fileUtils.js
@@ -1,32 +1,35 @@
 const fs = require("node:fs");
 
 const readFromFile = (fileName) => {
-  try {
-    const data = fs.readFileSync(fileName, "utf8");
+    let data;
+    try {
+        data = fs.readFileSync(fileName, "utf8");
+    } catch (err) {
+        // if file not found - create a new empty one
+        console.error(err);
+    }
     return data;
-  } catch (err) {
-    console.error(err);
-    throw err;
-  }
-};
+}
+
+
 const appendToFile = (fileName, data) => {
-  try {
-    fs.writeFileSync(fileName, data, { flag: "a" });
-    // file written successfully
-  } catch (err) {
-    console.error(err);
-    throw err;
-  }
+    try {
+        fs.writeFileSync(fileName, data, { flag: "a" });
+        // file written successfully
+    } catch (err) {
+        console.error(err);
+        throw err;
+    }
 };
 
 const overwriteFile = (fileName, data) => {
-  try {
-    fs.writeFileSync(fileName, data);
-    // file written successfully
-  } catch (err) {
-    console.error(err);
-    throw err;
-  }
+    try {
+        fs.writeFileSync(fileName, data);
+        // file written successfully
+    } catch (err) {
+        console.error(err);
+        throw err;
+    }
 };
 
 module.exports = { readFromFile, appendToFile, overwriteFile };
