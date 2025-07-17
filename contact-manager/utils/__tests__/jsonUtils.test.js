9566fdcdf530a6655979244281db489ab37569c2
itaiglikman <itaig1998@gmail.com>
2025-07-17 14:04:51 +0300

    testing jsonUtils

--- contact-manager/utils/__tests__/jsonUtils.test.js
+++ contact-manager/utils/__tests__/jsonUtils.test.js
@@ -1,6 +1,64 @@
-import jsonUtils from '../jsonUtils.js';
+const { JSONToObjectArray, ObjectArrayToJSON } = require('../jsonUtils.js');
 
-test("json to array",()=>{
-    const json = "[]";
-    expect(jsonUtils.JSONToObjectArray(json).toBe([]));
-})
+describe("check the functions that modify jsons to arrays and array to jsons", () => {
+
+    // JSONToObjectArray
+    describe("check JSONToObjectArray that parse received string json", () => {
+
+        it("should parse valid JSON arrays which contain only objects", () => {
+            expect(JSONToObjectArray('[]')).toEqual([]);
+            expect(JSONToObjectArray('[{"a":1}]')).toEqual([{ a: 1 }]);
+            expect(JSONToObjectArray('[{"a":1},{"bar":1},{}]')).toEqual([{ "a": 1 }, { "bar": 1 }, {}]);
+        });
+
+        it("should throw an error for invalid JSON", () => {
+            const invalidJsonInputs = [
+                undefined,
+                BigInt(9007199254740991),
+                '"hello"',
+                '123',
+                '',
+                '{foo:bar}',
+                '{"a":1}',
+                '{]',
+                '[{]',
+                '{"key":value}',
+                '[1, 2, 3',
+                'not a json',
+                'true',
+                'null',
+                true,
+                null,
+            ];
+            invalidJsonInputs.forEach((element) => {
+                expect(() => JSONToObjectArray(element)).toThrow();
+            });
+        });
+    });
+
+    // ObjectArrayToJSON
+    describe("check ObjectArrayToJSON that receives and array and stringify it", () => {
+
+        it("should stringify valid arrays and objects", () => {
+            expect(ObjectArrayToJSON([])).toBe('[]');
+            expect(ObjectArrayToJSON([{ a: 1 }])).toBe('[{"a":1}]');
+            expect(ObjectArrayToJSON([{ "a": 1 }, { "bar": 1 }, {}])).toBe('[{"a":1},{"bar":1},{}]');
+        });
+
+        it("should throw an error for invalid JSON", () => {
+            const invalidJsonInputs = [
+                undefined,
+                BigInt(9007199254740991),
+                [1, 2, 3],
+                { a: 1 },
+                "hello",
+                123,
+                true,
+                null,
+            ];
+            invalidJsonInputs.forEach((element) => {
+                expect(() => JSONToObjectArray(element)).toThrow();
+            });
+        });
+    });
+});
