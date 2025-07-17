04cefb64842a72b5fa3331e14cb4b01b74552253
itaiglikman <itaig1998@gmail.com>
2025-07-17 14:04:29 +0300

    fix: make functions treat invalid data jsons

--- contact-manager/utils/jsonUtils.js
+++ contact-manager/utils/jsonUtils.js
@@ -1,21 +1,25 @@
 const JSONToObjectArray = (json) => {
-  try {
-    const parsedJSON = JSON.parse(json);
-    return parsedJSON;
-  } catch (err) {
-    console.error(err);
-    throw err;
-  }
+    try {
+        const parsedJSON = JSON.parse(json);
+        if (!Array.isArray(parsedJSON) || !parsedJSON.every(obj => typeof obj === "object"))
+            throw new Error("invalid json");
+        return parsedJSON;
+    } catch (err) {
+        console.error(err);
+        throw err;
+    }
 };
 
 const ObjectArrayToJSON = (objectArray) => {
-  try {
-    const stringifiedObjectArray = JSON.stringify(objectArray);
-    return stringifiedObjectArray;
-  } catch (err) {
-    console.error(err);
-    throw err;
-  }
+    try {
+        if (!Array.isArray(objectArray) || !objectArray.every(obj => typeof obj === "object"))
+            throw new Error("invalid json");
+        const stringifiedObjectArray = JSON.stringify(objectArray);
+        return stringifiedObjectArray;
+    } catch (err) {
+        console.error(err);
+        throw err;
+    }
 };
 
 // ES6 export (keep for reference):
